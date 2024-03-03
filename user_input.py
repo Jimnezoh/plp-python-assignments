9344a863627c10fdd26782036e7d0a54a524cc0d
Jimnezoh <jimmaina100@gmail.com>
2024-03-03 13:12:21 +0300

    python wk1 assignment

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your full names")
+age = input("Enter your age")
+location = input("Enter your current location")
+print("hello " + name + " your current age is: " + age + " and your current location is : " + location )
